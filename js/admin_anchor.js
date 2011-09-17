d94e83efaa0a5ad7b024b6f321e4659533dcd603
Luciano Antonio Siqueira <lcnsqr@gmail.com>
2011-09-17 04:51:45 -0300

    Simplified function to detect anchor (url hash) changes

--- js/admin_anchor.js
+++ js/admin_anchor.js
@@ -5,22 +5,19 @@ var currentAnchor = null;
 
 $(function() {
 	// Anchor navigation
-	setInterval("checkAnchor()", 100);
+	setInterval(function() {
+		// Check if it has changes
+		if(currentAnchor != document.location.hash){
+			currentAnchor = document.location.hash;
+		}
+		
+		// Parse anchor functions
+		var i = 0;
+		for (i = 0; i < checkAnchorHook.length; i++)
+		{
+			eval(checkAnchorHook[i]);
+		}
+	}, 100);
 });
-
-function checkAnchor() {
-
-	// Check if it has changes
-	if(currentAnchor != document.location.hash){		
-		currentAnchor = document.location.hash;
-	}
-	
-	// Parse anchor functions
-	var i = 0;
-	for (i = 0; i < checkAnchorHook.length; i++)
-	{
-		eval(checkAnchorHook[i]);
-	}
-}
 
 //]]>
