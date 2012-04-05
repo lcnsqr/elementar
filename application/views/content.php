d7e5eb5936eee833b48330dec55a9cea964cb4bf
Luciano Antonio Siqueira <lcnsqr@gmail.com>
2012-04-05 02:02:03 -0300

    Frontend now suposed to be HTML5

--- application/views/content.php
+++ application/views/content.php
@@ -1,26 +1,18 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang; ?>" lang="<?php echo $lang; ?>">
-
+<!DOCTYPE html>
+<html lang="<?php echo $lang; ?>">
 <head>
-<title><?php echo $title; ?> &laquo; <?php echo $site; ?></title>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-
-<link rel="shortcut icon" href="<?php echo $favicon; ?>" />
-
+	<meta charset="utf-8">
+	<title><?php echo $title; ?> &laquo; <?php echo $site; ?></title>
+	<link rel="icon" href="<?php echo $favicon; ?>" />
 <?php foreach ($metafields as $metafield) : ?>
 <?php if ( (bool) $metafield['value'] ): ?>
-<meta name="<?php echo $metafield['name']; ?>" content="<?php echo $metafield['value']; ?>" /> 
+	<meta name="<?php echo $metafield['name']; ?>" content="<?php echo $metafield['value']; ?>" /> 
 <?php endif; ?>
 <?php endforeach; ?>
-
+	<link rel="stylesheet" href="/main/css/<?php echo $content_id; ?>" />
+	<script src="/main/javascript/<?php echo $content_id; ?>"></script>
 <?php echo $extra_head; ?>
-
-<link rel="stylesheet" href="/main/css/<?php echo $content_id; ?>" type="text/css" />
-<script type="text/javascript" src="/main/javascript/<?php echo $content_id; ?>"></script>
-
 </head>
-
 <body>
 <?php echo $content; ?>
 </body>
