0ef125593959ec76ffb5d0764bde2635efb2ecbe
Luciano Antonio Siqueira <lcnsqr@gmail.com>
2014-01-05 12:26:56 -0800

    Fixed tinymce reloading issue

--- js/backend/backend_content_tree.js
+++ js/backend/backend_content_tree.js
@@ -218,15 +218,15 @@ $(function() {
 			var editor = 'meta';
 		}
 		
-		// Remove CodeMirror instances
+		// Remove CodeMirror and tinymce instances
 		CodeMirrors.revert();
+		if ( typeof tinymce != "undefined" ) tinymce.remove();
 
 		$.post("/backend/editor/xhr_render_content_form", { id : id, editor : editor }, function(data){
 			if ( data.done == true ) {
 
 				$("#content_window").html(data.html).show(function() {
 					// WYSIWYG textarea activation
-					if ( typeof tinymce != "undefined" ) tinymce.remove();
 					$('#content_editor_form').find('textarea').each(function(){ $(this).wysiwyg(); });
 					
 					// Restore CodeMirror instances
@@ -251,11 +251,13 @@ $(function() {
 
 		var id = $(this).attr('href');
 
+		// Remove tinymce instances
+		if ( typeof tinymce != "undefined" ) tinymce.remove();
+
 		$.post("/backend/editor/xhr_render_element_form", { id : id }, function(data){
 			if ( data.done == true ) {
 				$("#content_window").html(data.html).show(function() {
 					// WYSIWYG textarea activation
-					if ( typeof tinymce != "undefined" ) tinymce.remove();
 					$('#content_editor_form').find('textarea').each(function(){ $(this).wysiwyg(); });
 				});
 			}
